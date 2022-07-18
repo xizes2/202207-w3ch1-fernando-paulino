ca34f84f54bfdc74573a2b44101870129908ed10
Fernando Paulino <xizes2@hotmail.com>
2022-07-18 19:22:31 +0200

    add js characters files, config their import/export

---
+++ public/js/components/Luchador.js
@@ -0,0 +1,34 @@
+import Personaje from "./Personaje.js";
+
+class Luchador extends Personaje {
+  arma;
+  destreza;
+
+  constructor(
+    nombreLuchador,
+    familiaLuchador,
+    edadLuchador,
+    armaLuchador,
+    valorDestreza
+  ) {
+    super(nombreLuchador, familiaLuchador, edadLuchador);
+    this.arma = armaLuchador;
+    this.destreza = this.filtraDestreza(valorDestreza);
+  }
+
+  filtraDestreza(valorDestreza) {
+    if (valorDestreza < 0) {
+      return 0;
+    } else if (valorDestreza > 10) {
+      return 10;
+    } else {
+      return valorDestreza;
+    }
+  }
+
+  comunicar() {
+    return `${super.comunicar()}Primero pego y luego pregunto`;
+  }
+}
+
+export default Luchador;
